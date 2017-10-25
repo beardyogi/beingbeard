ee675f524ced648d47a15a91b7177d868f9e3dcb
Beard Yogi <beardypgi@github.com>
2017-10-25 07:46:32 +0530

    Added DB to ignore

--- application/views/trending.php
+++ application/views/trending.php
@@ -1,3 +1,32 @@
-<div class="container">
-  <?php var_dump($tezt);?>
+<div class="container top-margin" >
+  <h3 class="text-center"><strong>TRENDING CAMPAIGNS</strong></h3>
+  <div class="row">
+  <?php
+  //print_r($content);
+  foreach ($content['campaigns'] as $key) {
+    ?>
+          <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
+              <div class="card">
+                  <div class="card-image" style="background-color:<?php echo $key['bg_color']?>">
+                      <img class="img-responsive campaign_image" src="<?php echo $base_url.$key['path'].$key['image_url']?>" alt="<?php echo $key['campaign_name']?>">
+                  </div><!-- card image -->
+
+                  <div class="card-content">
+                      <span class="card-title"><?php echo $key['campaign_name'] ?></span>
+                      <?php if ($this->ion_auth->logged_in()){?>
+                      <a href="<?php echo $base_url.'campaign/detail?id='.$key['id']?>"><button type="button" id="<?php echo strtolower($key['campaign_name']) ?>" class="btn btn-warning pull-right" aria-label="Left Align">
+                          Try
+                      </button></a>
+                    <?php }else{ ?>
+                      <a href="<?php echo $base_url.'auth'?>"><button type="button" id="<?php echo strtolower($key['campaign_name']) ?>" class="btn btn-warning pull-right" aria-label="Left Align">
+                          Try
+                      </button></a>
+                    <?php } ?>
+
+                  </div><!-- card content -->
+              </div>
+          </div>
+  <?php } ?>
+  </div>
+
 </div>
