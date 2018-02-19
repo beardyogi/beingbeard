3974b8f418c76df6fc29adc3ef0596b2e5629ca3
Beard Yogi <beardypgi@github.com>
2018-02-19 07:10:20 +0530

    feed crud

--- application/modules/admin/controllers/Feeds.php
+++ application/modules/admin/controllers/Feeds.php
@@ -6,7 +6,6 @@ class Feeds extends Admin_Controller {
 	public function index()
 	{
 		$crud = $this->generate_crud('feeds_source');
-        $crud->columns('id','campaign_name','bg_color','status','added_on');
         $this->mPageTitle = 'Feeds';
 		$this->render_crud();
     }
